11b5a407c930c6249aa8657c7b7a0aedb5b7f77d
SamCcode <yhfe22.karlsa@folkuniversitetet.nu>
2023-05-05 18:05:15 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
-"use strict";
 
-var _react = _interopRequireDefault(require("react"));
-var _client = _interopRequireDefault(require("react-dom/client"));
-var _App = _interopRequireDefault(require("./App"));
-require("./index.scss");
-var _redux = require("redux");
-var _reactRedux = require("react-redux");
-var _padelReducers = _interopRequireDefault(require("./reducers/padelReducers"));
-var _reactRouterDom = require("react-router-dom");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var store = (0, _redux.legacy_createStore)(_padelReducers["default"], window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-_client["default"].createRoot(document.getElementById('root')).render(
-/*#__PURE__*/
-// <React.StrictMode>
-_react["default"].createElement(_reactRedux.Provider, {
-  store: store
-}, /*#__PURE__*/_react["default"].createElement(_reactRouterDom.BrowserRouter, null, /*#__PURE__*/_react["default"].createElement(_App["default"], null)))
-// </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.scss";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import padelReducers from "./reducers/padelReducers";
+import { BrowserRouter } from "react-router-dom";
+
+const store = createStore(
+  padelReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
